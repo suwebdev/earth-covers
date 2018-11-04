b9acb063fc6e3a3eaa99691188137b09d8044faa
Kristi Short <shortk183@seattleu.edu>
2018-11-04 14:27:15 -0800

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+javascript:(function(){
+    document.getElementById('username').value='UserName';
+    document.getElementById('password').value='Password';
+    document.forms.loginForma.submit.click();
+  })();
