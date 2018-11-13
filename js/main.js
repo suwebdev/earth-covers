b4f4328e332ae84c16686565f6864b442ba1d251
Kristi Short <shortk183@seattleu.edu>
2018-11-12 19:25:43 -0800

    More changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-function validate() {
-    document.getElementById('username').value='UserName';
-    document.getElementById('password').value='Password';
-    document.forms.loginForma.submit.click();
-  })();
+<script language = "JavaScript">
+        function validate() {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+            if (username == null || username == "") {
+                alert("Please enter the username.");
+                return false;
+            }
+            if (password == null || password == "") {
+                alert("Please enter the password.");
+                return false;
+            }
+            alert('Login successful');
+            
+        } 
+</script>
